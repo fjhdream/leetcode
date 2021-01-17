d6d606b6f5186590a018a835871225ad772804fe
fjhdream <fjh_dream@163.com>
2021-01-17 21:46:46 +0800

    leetcode1232

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -1,2 +1,3 @@
 pub mod leetcode1018;
 pub mod leetcode947;
+pub mod leetcode1232;
---
+++ src/leetcode/leetcode1232.rs
@@ -0,0 +1,23 @@
+pub fn check_straight_line(coordinates: Vec<Vec<i32>>) -> bool {
+    let mut coordinates = coordinates; 
+    if coordinates.len() <= 2 {
+        return true;
+    }
+    let delta_x = coordinates[0][0];
+    let delta_y = coordinates[0][1];
+    let n = coordinates.len();
+    for idx in 0..n {
+        coordinates[idx][0] -= delta_x;
+        coordinates[idx][1] -= delta_y;
+    }
+    let A = coordinates[1][1];
+    let B = -coordinates[1][0];
+    for idx in 2..n {
+        let x = coordinates[idx][0];
+        let y = coordinates[idx][1];
+        if A * x + B * y != 0 {
+            return false;
+        }
+    }
+    true
+}
