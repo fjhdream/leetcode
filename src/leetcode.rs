32ee4055054b5a30026a8bb6940cca4f3482f788
fjhdream <fjh_dream@163.com>
2021-11-02 22:53:15 +0800

    leetcode

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -1,3 +1,4 @@
+mod leetcode441;
 mod leetcode517;
 mod leetcode583;
-mod leetcode441;
+mod leetcode66;
---
+++ src/leetcode/leetcode66.rs
@@ -0,0 +1,16 @@
+pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+    let n = digits.len();
+    for i in (0..n).rev() {
+        if digits[i] != 9 {
+            digits[i] += 1;
+            for j in i + 1..n {
+                digits[j] = 0;
+            }
+            return digits;
+        }
+    }
+    
+    let mut ans = vec![0; n + 1];
+    ans[0] = 1;
+    return ans;
+}
