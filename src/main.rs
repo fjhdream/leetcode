59f52ba694f3899a4998c10adcd537aabe880d09
fjhdream <fjh_dream@163.com>
2022-06-05 18:12:00 +0800

    G

--- src/main.rs
+++ src/main.rs
@@ -3,88 +3,93 @@
 #![allow(unused_variables)]
 use core::num;
 use std::cmp::*;
-use std::collections::{BinaryHeap, HashMap, VecDeque};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use std::io;
+use std::iter::FromIterator;
 use std::{io::prelude::*, io::*, str};
 
-#[derive(Debug, Clone, Eq, PartialEq)]
-struct tree_array {
-    trees: Vec<i64>,
-    n: usize,
-}
-
-impl tree_array {
-    fn new(n: usize) -> Self {
-        tree_array {
-            n,
-            trees: vec![0; n + 1],
-        }
-    }
-
-    fn low_bit(x: i64) -> i64 {
-        x & -x
-    }
-
-    fn add(&mut self, x: usize, data: i64) {
-        let mut i = x + 1;
-        while i <= self.n {
-            self.trees[i] += data;
-            i += Self::low_bit(i as i64) as usize;
-        }
-    }
-
-    fn query(&self, x: usize) -> i64 {
-        let mut i = x + 1;
-        let mut res = 0;
-        while i > 0 {
-            res += self.trees[i];
-            let diff = Self::low_bit(i as i64);
-            if diff >= i as i64 {
-                break;
-            }
-            i -= diff as usize;
-        }
-        res
-    }
-}
+const MOD: i64 = 1_000_000_007;
 
 fn solve(lines: &mut Lines<StdinLock>) -> Result<()> {
     let mut out = BufWriter::new(stdout());
-    let nums: Vec<i64> = lines
+    let inputs: Vec<i64> = lines
         .next()
         .unwrap()
         .unwrap()
         .split_whitespace()
         .map(|ch| ch.parse::<i64>().unwrap())
         .collect();
-    let n = nums[0] as usize;
-    let str = lines.next().unwrap().unwrap();
-    let mut str_chs = str.chars().collect::<Vec<_>>();
-    let mut res = 0i64;
-    let mut prefix_minus_num = vec![0i64; n + 1];
-    for (i, ch) in str_chs.iter().enumerate() {
-        prefix_minus_num[i + 1] = prefix_minus_num[i] + if *ch == '-' { 1 } else { -1 };
-    }
-    for m in 0..3 {
-        let mut tree_array = tree_array::new(2 * n + 1);
-        for i in 0..=n {
-            if (prefix_minus_num[i] - m) % 3 == 0 {
-                res += tree_array.query((prefix_minus_num[i] + n as i64) as usize);
-                tree_array.add((n as i64 + prefix_minus_num[i]) as usize, 1);
+    let (n, p) = (inputs[0], inputs[1]);
+    let mut nums = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split_whitespace()
+        .map(|ch| ch.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    let mut duplicate_map: HashMap<i64, bool> =
+        HashMap::from_iter(nums.iter().map(|&x| (x, false)));
+    // 去除给定集合a 中的可以转换元素
+    for &num in nums.iter().rev() {
+        if !*duplicate_map.get(&num).unwrap() {
+            let mut tmp = num;
+            while tmp > 0 {
+                let mut trans = false;
+                if tmp & 1 == 1 {
+                    trans = true;
+                    tmp >>= 1;
+                } else if tmp % 4 == 0 {
+                    trans = true;
+                    tmp >>= 2;
+                } else {
+                    break;
+                }
+                if trans && duplicate_map.contains_key(&tmp) {
+                    duplicate_map.insert(num, true);
+                    break;
+                }
             }
         }
     }
+
+    let mut f = vec![0i64; 200000 + 1];
+    for (key, val) in duplicate_map.iter() {
+        if !val {
+            f[count_bit(*key)] += 1;
+        }
+    }
+
+    let mut res = 0i64;
+    f[1] = f[0] + f[1];
+    f[2] = f[1] + f[0] + f[2];
+    res = (res + f[2] + f[1]) % MOD;
+
+    for i in 3..=p as usize {
+        f[i] = (f[i] + f[i - 1] + f[i - 2]) % MOD;
+        res = (res + f[i]) % MOD;
+    }
+
     writeln!(out, "{}", res)?;
     Ok(())
+}
+
+fn count_bit(x: i64) -> usize {
+    let mut res = 0;
+    let mut tmp = x;
+    while tmp > 0 {
+        res += 1;
+        tmp >>= 1;
+    }
+    res
 }
 
 fn main() -> Result<()> {
     let stdin = stdin();
     let mut lines = stdin.lock().lines();
 
-    let tc = lines.next().unwrap().unwrap().parse::<usize>().unwrap();
-    for _ in 0..tc {
-        solve(&mut lines)?;
-    }
+    // let tc = lines.next().unwrap().unwrap().parse::<usize>().unwrap();
+    // for _ in 0..tc {
+    solve(&mut lines)?;
+    // }
     Ok(())
 }
